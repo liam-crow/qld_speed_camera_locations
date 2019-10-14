5364a5552f57ac0877651923fb735e181d853836
liam <liam.m.crowhurst@gmail.com>
2019-10-14 22:38:42 +1000

    Lat and long for suburbs

--- speed_cameras_exploration.R
+++ speed_cameras_exploration.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(stringr)
 
 # https://www.data.qld.gov.au/dataset/active-mobile-speed-camera-sites/resource/f6b5c37e-de9d-4041-8c18-f4d4b6c593a8
 mobile_data <- read.csv("https://www.data.qld.gov.au/dataset/0fffea7f-6988-4b73-afee-5aa8af56afd2/resource/f6b5c37e-de9d-4041-8c18-f4d4b6c593a8/download/qpsactive_parkedmscamera.csv")
@@ -6,7 +7,18 @@ mobile_data <- read.csv("https://www.data.qld.gov.au/dataset/0fffea7f-6988-4b73-
 # https://www.data.qld.gov.au/dataset/active-mobile-speed-camera-sites/resource/d059503f-3685-4669-8c43-df5b74da8ba8
 trailer_data <- read.csv("https://www.data.qld.gov.au/dataset/0fffea7f-6988-4b73-afee-5aa8af56afd2/resource/d059503f-3685-4669-8c43-df5b74da8ba8/download/rsct_sites.csv")
 
-street_data <- read.csv("Road location and traffic data_20190910.txt")
+suburb_lat_long <- read.csv("Australian_Post_Codes_Lat_Lon.csv") %>% filter(state == "QLD")
+
+# street_data <- read.csv("Road location and traffic data_20190910.txt")
+# sd_filt <- street_data %>% 
+#   filter(Longitude >= 152.5) %>% 
+#   sample_frac(0.1)
+# 
+# # par(pty="s")
+# with(
+#   sd_filt,
+#   plot(Longitude, Latitude)
+# )
 
 head(trailer_data)
 head(mobile_data)
@@ -23,4 +35,14 @@ merge_locations <- rbind(mobile_data, trailer_data)
 
 head(merge_locations)
 
-merge_split <- merge_locations %>% tidyr::separate(location, c("Street","Suburb"), sep = ", ", remove = F)
+merge_split <- merge_locations %>% tidyr::separate(location, c("street","suburb"), sep = ", ", remove = F)
+
+merge_split$suburb <- str_to_lower(merge_split$suburb)
+suburb_lat_long$suburb <- str_to_lower(suburb_lat_long$suburb)
+
+loc_join <- merge_split %>% left_join(suburb_lat_long, by = c("suburb"))
+
+library(leaflet)
+leaflet(data = loc_join[200:400,]) %>% addTiles() %>% 
+  addMarkers(~lon+rnorm(201,sd=0.005), ~lat+rnorm(201,sd=0.005), popup = ~as.character(location), label = ~as.character(location))
+
